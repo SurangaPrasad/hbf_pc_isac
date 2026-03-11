d559d5a1a7d811a7cba7a3e3c8b6d44154861129
surangaprasad55@gmail.com <surangaprasad55@gmail.com>
2026-03-11 17:32:23 +0200

    updating the training data size to 1024

--- system_config.py
+++ system_config.py
@@ -64,11 +64,11 @@ system_info = str(Nt) + " Tx antennas, " + str(M) + " users, " + str(Nrf) + " RF
 print(system_info)
 
 # ////////////////////////////////////////////// MODEL PARAMS //////////////////////////////////////////////
-train_size = 256    # size of training set
+train_size = 1024    # size of training set
 test_size = 1      # size of testing set
 batch_size = 32     # batch size when training (increased from 16 for better GPU utilisation)
 n_epoch = 30         # number of training epochs
-learning_rate = 0.0001 # learning rate
+learning_rate = 0.00001 # learning rate
 
 n_iter_outer = 120      # Number of outer iterations (I)
 n_iter_inner_J5 = 5     # Number of inner iterations (J = 5)
