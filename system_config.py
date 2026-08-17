dbde327f9eedd7918219f4d91b4a8d915b074a2f
Suranga Wengappuli Arachchige <surangaprasad55@gmail.com>
2026-08-17 23:32:13 +0300

    setting the config to train selectionNet

--- system_config.py
+++ system_config.py
@@ -64,12 +64,12 @@ OMEGA = 0.25
 
 
 # ////////////////////////////////////////////// MODEL PARAMS //////////////////////////////////////////////
-train_size = 112 * 4    # size of training set
+train_size = 112 * 4 * 10   # size of training set
 if str(device) == 'cuda':
     test_size = 50     
 else:
     test_size = 30     
-batch_size = len(snr_dB_list) * 4
+batch_size = len(snr_dB_list) * 4 * 40
 n_epoch = 30         # number of training epochs
 learning_rate = 0.005 # learning 
 # learning_rate = 0.00002
@@ -150,8 +150,8 @@ if data_source == 'python':
     train_data_file_name = "train_data.mat"
     test_data_file_name = "test_data.mat"
 else:  # matlab
-    train_data_file_name = "train_data_matlab.mat"
-    # train_data_file_name = "H_train.mat"
+    # train_data_file_name = "train_data_matlab.mat"
+    train_data_file_name = "H_train.mat"
     test_data_file_name = "test_data_matlab.mat"
 
 data_path_train = directory_data + train_data_file_name
